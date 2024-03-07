3debfe8002c32cb36cbe3130eb2774899a78fc6e
Ahemd <ahmedsaye23156@gmail.com>
2024-03-07 22:49:08 +0200

    .

---
+++ cs- harvard/hi.py
@@ -0,0 +1,12 @@
+def check(x,y):
+    if x < y : 
+        print("x is less than y")
+    elif x > y :
+        print('x is greater than y ')
+    else:
+        print('x is equal to y!')
+
+
+
+
+(check(12,12+  1 - 1 )) # here is the way you always can do it better i don't care, i just believe in you, we can pay the best offer never ever, we can 
