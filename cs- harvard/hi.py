993af2c8d279697a72e7a8f096bc21b539679078
Ahemd <ahmedsaye23156@gmail.com>
2024-03-08 10:07:25 +0200

    .

--- cs- harvard/hi.py
+++ cs- harvard/hi.py
@@ -1,6 +1,15 @@
+def main():
+    x  = input("Enter a number: ")
+    if is_even(int(x)):
+        print("Even")
+    else:
+        print("ODD")
+
+
+def is_even(ex):
+    return True if ex % 2 == 0 else False
+    
 
 
 
-
-
-
+main()
