d672b137737d1453a7853ab88d0e66c1a7c0d7ce
Ahemd <ahmedsaye23156@gmail.com>
2024-03-08 23:28:55 +0200

    .

--- cs- harvard/hi.py
+++ cs- harvard/hi.py
@@ -1,16 +1,2 @@
-students = {
-    "Hermoine": "Gryffindor",
-    "Harry": "Gryffindor",
-    "Ron": "Gryffindor",
-    "Draco": "Slytherin",
-}
-for student,n in enumerate(students):
-    print(n[student])
-
-
-
-
-
-
 
 
